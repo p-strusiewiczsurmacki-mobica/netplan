9852fef179779dcd69d00d2702e7fc0dda68630e
Patryk Strusiewicz-Surmacki <patryk-pawel.strusiewicz-surmacki@external.telekom.de>
2025-01-29 14:56:00 +0100

    test?
    Signed-off-by: Patryk Strusiewicz-Surmacki <patryk-pawel.strusiewicz-surmacki@external.telekom.de>

--- tests/generator/base.py
+++ tests/generator/base.py
@@ -185,10 +185,7 @@ class NetplanV2Normalizer():
             scalars_only = not any(list(map(lambda elem: (isinstance(elem, dict) or isinstance(elem, list)), data)))
 
             # sort sequence alphabetically
-            if scalars_only:
-                for i in range(len(data)):
-                    data[i] = str(data[i])
-
+            if scalars_only and all(type(x) == type(data[0]) for x in data):
                 data.sort()
                 # remove duplicates (if needed)
                 unique = set(data)
